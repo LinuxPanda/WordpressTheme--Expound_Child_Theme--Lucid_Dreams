089a55b5a6af36d421710761708f77d231ff42d0
LinuxPanda <LinuxPanda@users.noreply.github.com>
2014-04-10 16:52:50 +0530

    genericons are enabled by default
    to add support for that square bullet before list items in sidebar

--- functions.php
+++ functions.php
@@ -153,16 +153,14 @@ function override_expound_scripts() {
 
 	wp_enqueue_script( 'expound-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_style( 'expound-genericons', get_template_directory_uri() . '/css/genericons.css', array(), '20140127' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
 		wp_enqueue_script( 'expound-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
-
-	if ( has_nav_menu( 'social' ) ) {
-		wp_enqueue_style( 'expound-genericons', get_template_directory_uri() . '/css/genericons.css', array(), '20140127' );
 	}
 }
 endif; // expound_scripts
